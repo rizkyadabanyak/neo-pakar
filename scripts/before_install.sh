087f7231ff4dd2d5c313aabc5585bf006556b581
rizkyadabanyak <riskipatra5@gmail.com>
2023-11-03 14:55:54 +0700

    migaration

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -14,7 +14,7 @@ if [ ! command -v nvm &> /dev/null ]; then
 fi
 
 # CEK DIREKTORI
-DIR="/home/ubuntu/backend"
+DIR="/home/ubuntu/backend_neo"
 echo "CEK DIRECTORY $DIR"
 if [ -d "$DIR" ]; then
   echo "$DIR TELAH ADA"
